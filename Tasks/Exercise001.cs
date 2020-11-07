a14957281d91d25794e85837b3f078af9e8024e0
chiwong08 <chiandwendy@gmail.com>
2020-11-07 16:12:27 +0000

    added comments to show thoughts on how the function was conceived

--- Tasks/Exercise001.cs
+++ Tasks/Exercise001.cs
@@ -15,12 +15,18 @@ namespace TechReturners.Tasks
     {
         public static int Singles(List<int> source)
         {
+            // found on Google a dictionary which takes in an array as input to 
+            // create a key-value pair of the occurences of each element in the array
+            // so converting the initial List to array
+
             int[] intArray = source.ToArray();
 
             Dictionary<int, int> counts = intArray
                 .GroupBy( num => num )
                 .ToDictionary(num => num.Key, num => num.Count());
 
+            // now that we have a dictionary of key-value pairs, we can find the ones that only occur once
+            // and add to a running total (which is initialised to zero)
             int total=0;
 
             foreach (KeyValuePair<int, int> item in counts) {
